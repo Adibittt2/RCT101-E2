45b69e81a2e292cc66bc68e50b2934ffa405f4e7
Adibittt2 <adityaverma00796@gmail.com>
2022-05-30 19:31:20 +0530

    commitment

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -10,12 +10,32 @@ const AddProduct = () => {
   const RadioGroup = () => <div />;
   const Radio = () => <div />;
 
+  const [val,setVal]= useState("");
+  const [cli,setCli]= useState();
+
+  const clickFun =(()=>{
+
+    setCli(()=>{
+
+      return(
+        setCli(val)
+      )
+    })
+
+    setVal("");
+  })
+
+  const changeFun =(event)=>{
+
+    setVal(event.target.value);
+  }
+
   return (
     <>
       <Button my={4} data-cy="add-product-button"></Button>
       <Modal>
         <ModalBody pb={6}>
-          <Input data-cy="add-product-title" />
+          <Input data-cy="add-product-title" onChange = {changeFun} value={val} />
           <Select data-cy="add-product-category">
             <option data-cy="add-product-category-shirt"></option>
             <option data-cy="add-product-category-pant"></option>
@@ -26,8 +46,8 @@ const AddProduct = () => {
             <Radio data-cy="add-product-gender-female"></Radio>
             <Radio data-cy="add-product-gender-unisex"></Radio>
           </RadioGroup>
-          <Input data-cy="add-product-price" />
-          <Button data-cy="add-product-submit-button"></Button>
+          <Input data-cy="add-product-price"onChange = {changeFun} value={val} />
+          <Button data-cy="add-product-submit-button" onClick ={clickFun}></Button>
         </ModalBody>
       </Modal>
     </>
